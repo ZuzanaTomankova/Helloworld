c75c54ee038740dd1b6058c031d41614cab9538b
Zuzana Tománková <Tomankova.Z@seznam.cz>
2025-02-22 14:39:32 +0100

    první pokus

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,34 @@
 package org.example;
 
+//Zdrojový kód
+
+/**
+ * Popis metody, která následuje
+ * popisky lze automaticky vygenerovat do dokumentace
+ */
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
+        System.out./*vsuvka vrámci řádku*/println("Ahoj světe, jak se všichni máte?"); //Komentáře (naše poznámky)
+        System.out.println("To je fajn, že nemusím psát v poznámkovém bloku,,,");
+        /*
+         *   víceřádkový komentář
+         * Když nestačí jeden řádek
+         */
+        // cyklus for //breakpoint
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            System.out.print("i = " + i);
+            if (i < 5) {
+                System.out.println(", ");
+            }
         }
+        System.out.println();
     }
 }
+
+
